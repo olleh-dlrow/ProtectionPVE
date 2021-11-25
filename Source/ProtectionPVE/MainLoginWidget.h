e221ad1af6145ab325f80780117a5d18e4b3d92a
olleh-dlrow <1136246408@qq.com>
2021-11-25 23:40:47 +0800

    network modified

---
+++ Source/ProtectionPVE/MainLoginWidget.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MainLoginWidget.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROTECTIONPVE_API UMainLoginWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
---
+++ client.bat
@@ -0,0 +1,1 @@
+"E:\program_files\UE4Source\UnrealEngine\Engine\Binaries\Win64\UE4Editor.exe" "%CD%\ProtectionPVE.uproject" 127.0.0.1 -game -log -nosteam
